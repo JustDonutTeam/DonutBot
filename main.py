fda81c2381b8b58fcf54c73e3f7aa8600dcc42cc
Feeeeddmmmeee <wislocki.janek@gmail.com>
2020-10-13 19:55:15 +0200

    minor changes
    deleted some useless files

--- main.py
+++ main.py
@@ -95,4 +95,19 @@ async def icon(ctx):
 async def rules(ctx):
     await ctx.send(":wave: Welcome to the support server of Donut. Before taking any action please read the rules\n\n> - **The Rules**\n> These are the guidelines you have to follow in this server.\n> - **No Spam**\n> Any kind of spam will be punished\n> - **Absolutely no NSFW**\n> Every attempt at sending NSFW content will be punished with a kick or worse.\n> - **No Harrassement**\n> Everyone is equally good and you musn't harrass anyone.\n> - **Personal Information**\n> Without permission of the person, you are not allowed to reveal and share their personal information, such as name, adress, age, etc.\n> - **Argumentative Situations**\n> Anybody causing argumentative situations or drama can result in Administrative Action being taken against themselves.\n\n:link: **Links:**\n\nTop.gg: https://mub.me/topggDonut\nDiscord Bots: https://discord.bots.gg/bots/738788356506386462")
 
+@client.command(aliases=['bs'])
+async def botstats(ctx):
+
+    embed = discord.Embed(color=discord.Colour.from_rgb(255, 158, 253), timestamp=ctx.message.created_at)
+
+    embed.set_author(name=f'Bot Stats')
+    embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/646009680241623070/740564170964992130/donut-pfp.png')
+    embed.set_footer(text=f'Requested by {ctx.author}', icon_url=ctx.author.avatar_url)
+        
+    embed.add_field(name='Servers:', value=len(client.guilds))
+    embed.add_field(name='Library:', value='discord.py 1.5.0')
+    embed.add_field(name='ping:', value=f'{round(client.latency * 1000)}ms')
+
+    await ctx.send(embed=embed)
+
 client.run('NzM4Nzg4MzU2NTA2Mzg2NDYy.XyRARg.GaBbvmB3XhvzEDDhG8tWgfpJHFg')
